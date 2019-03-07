f16b19dd5b1b2d13d785f989713a81d6d53384f2
Dvontre Coleman <dvontrec@gmail.com>
2019-03-07 16:36:53 -0500

    initail commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,8 @@
 import React, { Component } from 'react';
-import { Route, HashRouter, IndexRoute } from 'react-router-dom';
-
-import Home from './Home';
-import Away from './Away';
-import Nav from './Nav';
 
 class App extends Component {
   render() {
-    return (
-      <HashRouter>
-        <div>
-          <Nav />
-          <Route exact path="/" component={Home} />
-          <Route path="/away" component={Away} />
-        </div>
-      </HashRouter>
-    );
+    return 'hello world';
   }
 }
 
