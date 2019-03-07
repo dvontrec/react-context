c16260c14c12b3e6e98d0efca35a61a88487da92
Dvontre Coleman <dvontrec@gmail.com>
2019-03-07 16:51:27 -0500

    Setup app basics

--- src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-
-const rootReducer = combineReducers({
-  // remove this when there are reducers to alter state
-  state: (state = {}) => state
-});
-
-export default rootReducer;
