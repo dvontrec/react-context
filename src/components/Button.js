4cd575d8d2d995dd65ff0ea615c95c95dd32a7d4
Dvontre Coleman <dvontrec@gmail.com>
2019-03-08 09:51:02 -0500

    Add default context object functionality

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,8 +1,15 @@
 import React, { Component } from 'react';
+// import context
+import LanguageContext from '../contexts/LanguageContext';
 
 class Button extends Component {
+  // Hooks up context object to class based component
+  static contextType = LanguageContext;
+
   render() {
-    return <button className="ui button primary">Submit</button>;
+    // if the context is set to english text is submit, if not it is voorleggen
+    const text = this.context === 'english' ? 'Submit' : 'Voorleggen';
+    return <button className="ui button primary">{text}</button>;
   }
 }
 
--- src/components/Field.js
+++ src/components/Field.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
 
+import LanguageContext from '../contexts/LanguageContext';
+
 class Field extends Component {
+  // assigns context from language context object
+  static contextType = LanguageContext;
   render() {
+    // set the text variable to be set based on context
+    const text = this.context === 'english' ? 'Name' : 'Naam';
     return (
       <div className="ui field">
-        <label>Name</label>
+        <label>{text}</label>
         <input />
       </div>
     );
